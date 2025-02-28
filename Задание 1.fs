e604448c1a9ab8cd920e4e3abaa335009bf345e0
Vik-11 <vikkuznetsov11@gmail.com>
2025-02-28 10:56:12 +0500

    Create Задание 1.fs

---
+++ Задание 1.fs
@@ -0,0 +1,28 @@
+open System
+
+let getCh () = 
+    let rec loop () =
+        printfn "Enter one symbol: "
+        let c = Console.ReadLine()
+        if c.Length = 1 then
+            c
+        else
+            printfn "Input error: Enter exactly one symbol"
+            loop()
+    loop()
+
+let readS () =
+   printfn "Enter string with space in between: "
+   let inp = Console.ReadLine()
+   inp.Split(' ')
+   |> Array.filter (fun s -> not (String.IsNullOrWhiteSpace(s)))
+   |> Array.toList
+
+let prepSymb (ch:string) strings =
+    strings |> List.map (fun s -> ch + s)
+
+let str = readS()
+let symb = getCh()
+let newL = prepSymb symb str
+
+printfn "%A" newL
